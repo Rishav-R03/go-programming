fd2a10f2d495d963f601c72cfc1c11daa8cde2e4
Rishav-R03 <rishav042023@gmail.com>
2026-08-10 15:09:36 +0530

    added write result error management, append rows logic

--- quizapp/internal/csv/result_writer.go
+++ quizapp/internal/csv/result_writer.go
@@ -15,23 +15,29 @@ type Header struct {
 }
 
 func WriteResult(path string, name string, rollno string, score int) error {
-	file, err := os.Create(path)
-	if err != nil {
-		return fmt.Errorf("error writing file: %w", err)
-	}
+	//Check if file exist
+	_, osStatusErr := os.Stat(path)
+	fileExists := !os.IsNotExist(osStatusErr)
 
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		return fmt.Errorf("error opening file: %w", err)
+	}
 	defer file.Close()
+
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
-	header := Header{Name: "Name", RollNo: "RollNo", Score: "Score"}
-	record := model.Result{Name: name, RollNO: rollno, Score: score}
-
-	if err := writer.Write([]string{header.Name, header.RollNo, header.Score}); err != nil {
-		return fmt.Errorf("cannot write: %w", err)
+	if !fileExists {
+		header := Header{Name: "Name", RollNo: "RollNo", Score: "Score"}
+		if err := writer.Write([]string{header.Name, header.RollNo, header.Score}); err != nil {
+			return fmt.Errorf("cannot write header: %w", err)
+		}
 	}
+
+	record := model.Result{Name: name, RollNO: rollno, Score: score}
 	if err := writer.Write([]string{record.Name, record.RollNO, strconv.Itoa(record.Score)}); err != nil {
-		return fmt.Errorf("cannot write: %w", err)
+		return fmt.Errorf("cannot write record: %w", err)
 	}
 	return nil
 }
--- quizapp/internal/serve_to_client/serve_to_client.go
+++ quizapp/internal/serve_to_client/serve_to_client.go
@@ -3,6 +3,7 @@ package servetoclient
 import (
 	"fmt"
 	"quizapp/internal/model"
+	"time"
 )
 
 func ConductQuiz(probChan <-chan model.MathProblem) (string, string, int, error) {
@@ -16,20 +17,36 @@ func ConductQuiz(probChan <-chan model.MathProblem) (string, string, int, error)
 	fmt.Scanf("%s\n", &rollNo)
 
 	for problem := range probChan {
-		fmt.Printf("\nQuestion: %d: %s => ", questionNum, problem.Problem)
-		var input int
-		_, err := fmt.Scanf("%d\n", &input)
-		if err != nil {
-			fmt.Println("Invalid input. Skipped question")
-			questionNum++
-			continue
-		}
+		fmt.Printf("\nQuestion %d: %s => ", questionNum, problem.Problem)
+		answerChan := make(chan int)
+		errorChan := make(chan error)
 
-		if VerifySolutionUpdateScore(problem.Solution, input) {
-			fmt.Println("Correct.")
-			score++
-		} else {
-			fmt.Printf("Incorrect! (correct answer: %d)", problem.Solution)
+		timer := time.NewTimer(5 * time.Second)
+
+		go func() {
+			var input int
+			_, err := fmt.Scanf("%d\n", &input)
+			if err != nil {
+				errorChan <- err
+				return
+			}
+			answerChan <- input
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println("\nTime's up! Question Skipped")
+		case err := <-errorChan:
+			timer.Stop()
+			fmt.Printf("\nInvalid input! Question skipped. %v", err)
+		case input := <-answerChan:
+			timer.Stop()
+			if VerifySolutionUpdateScore(problem.Solution, input) {
+				fmt.Printf("Correct")
+				score++
+			} else {
+				fmt.Printf("Incorrect! (correct answer: %d)\n", problem.Solution)
+			}
 		}
 		questionNum++
 	}
